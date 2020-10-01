8f2bced89d2478a10011afd020e3b72e086d1c18
Zachary Perkins <ezmac028@gmail.com>
2020-10-01 23:09:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,11 @@
-function basicTeenager(age) {
-
+function basicTeenager(> 12 && < 20) {
+  return("You are a teenger!")
 }
 
 function teenager(age) {
-
+  if(age){> 12 && < 20
+} return("You are a teenager!")
+} else if (true) {return("You are not a teenager!")
 }
 
 function ageChecker(age) {
