295e9fcb0b9b8f68e87384def0248e9a800b3680
Zachary Perkins <ezmac028@gmail.com>
2020-10-01 23:44:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -8,7 +8,9 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age >=13 && age <=19) {return "You are a teenager!"}
+  else if (age <=12) {return "You are a kid"}
+  else (age >19) {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
